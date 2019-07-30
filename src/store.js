c1ebaf587afd593978d08e7e8a586dba1fee9a1e
smirnoffnew <smirnoffnew@gmail.com>
2019-07-30 08:56:17 +0300

    finish

--- src/store.js
+++ src/store.js
@@ -3,23 +3,23 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 
 function saveToLocalStorage(state) {
-  // try {
-  //   const serializedState = JSON.stringify(state);
-  //   localStorage.setItem("state", serializedState);
-  // } catch (e) {
-  //   console.log(e);
-  // }
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function loadFromLocalStorage() {
-  // try {
-  //   const serializedState = localStorage.getItem("state");
-  //   if (serializedState === null) return undefined;
-  //   return JSON.parse(serializedState);
-  // } catch (e) {
-  //   console.log(e);
-  //   return undefined;
-  // }
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
 }
 
 const persistedState = loadFromLocalStorage();
